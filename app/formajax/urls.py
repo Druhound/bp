7cf02562dd8430a0919dcef01a7388e05f3a8271
druhound <Druhound51@gmail.com>
2017-02-07 18:34:10 +0300

    beta 0.3

--- app/formajax/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-
-from app.feedback_form.views import FeedbackCreateView
-
-urlpatterns = [
-    url(r'^ajax/(?P<url>.*)$', FeedbackCreateView.as_view(), name='feedback'),
-]
