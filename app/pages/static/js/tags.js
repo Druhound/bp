4ee8372a9663d1a9991cd918e6cb0a2780962892
druhound <Druhound51@gmail.com>
2017-02-13 18:24:13 +0300

    beta 0.4

--- app/pages/static/js/tags.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('#menu').find('#tag').hover(function() {
-        $(this).addClass('menu-l');
-        $('#sub-menu',this).stop().slideDown('1000');
-    },function() {
-        $(this).removeClass('menu-l');
-        $('#sub-menu',this).stop().slideUp('1000');
-    });
-});
