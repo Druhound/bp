39585b175404f77fbea8b14eabf4e7149b255259
Druhound <Druhound51@gmail.com>
2019-11-01 14:54:42 +0300

    remove trash

--- hello.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-
-def short_story():
-    print("У попа была собака, он ее любил.")
-    print("Она съела кусок мяса, он ее убил,")
-    print("В землю закопал и надпись написал:")
-    print("hello")
-
-short_story()
